e2dcbc656f262ecb5af65342e39c5af09e12502f
fmontoyadls <fmontoyadls@gmail.com>
2025-06-05 15:07:59 +0200

    Libro

--- KAZUKI/Assets/Scripts/PerfectTimingGame/TimingZone.cs
+++ KAZUKI/Assets/Scripts/PerfectTimingGame/TimingZone.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class TimingZone : MonoBehaviour
 {
     public RectTransform indicator;
     public RectTransform perfectZone;
     public TMP_Text resultText;
+    public string nextSceneName = "Nivel 2b";
+
+    public IndicatorMover indicatorMover; // <-- Referencia al otro script
+
+    private int streak = 0;
+
+    public float speedIncrease = 50f; // cuánto sube la velocidad por acierto
 
     void Update()
     {
@@ -17,11 +25,35 @@ public class TimingZone : MonoBehaviour
             float zoneWidth = perfectZone.rect.width / 2;
 
             if (distance < zoneWidth * 0.3f)
-                resultText.text = "🌟 PERFECT!";
+            {
+                resultText.text = "PERFECTO";
+                streak++;
+                IncreaseSpeed();
+            }
             else if (distance < zoneWidth)
-                resultText.text = "👍 Good";
+            {
+                resultText.text = "Bien";
+                streak++;
+                IncreaseSpeed();
+            }
             else
-                resultText.text = "❌ Bad";
+            {
+                resultText.text = "Mala";
+                streak = 0;
+            }
+
+            if (streak >= 3)
+            {
+                LevelLoader.LoadLevel(nextSceneName);
+            }
+        }
+    }
+
+    void IncreaseSpeed()
+    {
+        if (indicatorMover != null)
+        {
+            indicatorMover.speed += speedIncrease;
         }
     }
 }
